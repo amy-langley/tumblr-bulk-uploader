2c571332fbee7104a23c5b9b8fe5cc9890f80a7a
Amy Boyer <amy.r.boyer@gmail.com>
2016-02-14 16:40:59 -0600

    enumerate files in selected directory

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 #!/usr/bin/env node
-console.log('Hello, world!');
+var fs = require('fs')
+var path = require('path')
+var app = require('commander')
+var tumblr = require('tumblr')
+
+app
+  .arguments('<directory')
+  .option('-u, --username <username>')
+  .option('-t, --tag <tag>')
+  .action(function(directory){
+    processDirectory(directory)
+  })
+  .parse(process.argv)
+
+function processDirectory(directory){
+  fs.readdir(directory, function(err, files){
+    for(file of files)
+      uploadFile(path.join(directory,file))
+  })
+}
+
+function uploadFile(path){
+  console.log(path)
+}
