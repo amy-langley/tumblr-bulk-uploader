891e28a5ca327fed3fa8c3c8b4e3f22b685fe1d1
Amy Boyer <amy.r.boyer@gmail.com>
2016-02-14 18:12:28 -0600

    geez it's hard to even get an api call working

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var fs = require('fs')
 var path = require('path')
 var app = require('commander')
 var tumblr = require('tumblr')
-var jsonfile = require('jsonfile');
-var co = require('co');
-var prompt = require('co-prompt');
+var jsonfile = require('jsonfile')
+var co = require('co')
+var prompt = require('co-prompt')
+var oauth = require('oauth')
+var tumblr = require('tumblr')
+var t2 = require('tumblrwks')
 
-var consumerInfo = jsonfile.readFileSync(path.join(__dirname, './consumer.json'))
 
 app
   .arguments('<directory')
@@ -15,12 +17,19 @@ app
   .option('-t, --tag <tag>')
   .action(function(directory){
     co(function* (){
-      if(!app.username)
-        app.username = yield prompt('username: ')
 
-      app.password = yield prompt.password('password: ')
+      var consumerInfo = jsonfile.readFileSync(path.join(__dirname, './consumer.json'))
+      app.consumer_key = consumerInfo.key
+      app.consumer_secret = consumerInfo.secret
 
-      processDirectory(directory)
+      // if(!app.username)
+        // app.username = yield prompt('username: ')
+
+      // app.password = yield prompt.password('password: ')
+
+      // processDirectory(directory)
+      // authenticate()
+      try{ simple() } catch(x) { console.log(x) }
     })
   })
   .parse(process.argv)
@@ -34,4 +43,34 @@ function processDirectory(directory){
 
 function uploadFile(path){
   console.log(path)
+}
+
+function authenticate(andThen){
+  console.log('building auth request')
+  var oauth = new OAuth.OAuth(
+    'https://www.tumblr.com/oauth/request_token',
+    'https://www.tumblr.com/oauth/access_token',
+    consumerInfo.key,
+    consumerInfo.secret,
+    '1.0A',
+    null,
+    'HMAC-SHA1'
+  )
+  console.log('getting')
+  oauth.get(
+    'https://www.tumblr.com/oauth/authorize',
+    app.username,
+    app.password,
+    function(e, data, res){
+      console.log(res)
+    }
+  )
+}
+
+function simple() {
+  var tumb = new t2({consumerKey: app.consumer_key})
+  tumb.get('/info', {hostname: 'aetherstragic.tumblr.com'}, function(err, json){
+    console.log(err)
+    console.log(json)
+  })
 }
