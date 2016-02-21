d54d29fffad967e00bd8625c035e7049487cdf56
Amy Boyer <amy.r.boyer@gmail.com>
2016-02-21 17:42:06 -0600

    basic stuff working using pregenerated token and token secret

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 var fs = require('fs')
 var path = require('path')
 var app = require('commander')
-var tumblr = require('tumblr')
 var jsonfile = require('jsonfile')
 var co = require('co')
 var prompt = require('co-prompt')
 var oauth = require('oauth')
-var tumblr = require('tumblr')
-var t2 = require('tumblrwks')
+var tumblr = require('tumblr.js')
 
 
 app
@@ -19,13 +17,14 @@ app
     co(function* (){
 
       var consumerInfo = jsonfile.readFileSync(path.join(__dirname, './consumer.json'))
-      app.consumer_key = consumerInfo.key
-      app.consumer_secret = consumerInfo.secret
+      var token = jsonfile.readFileSync(path.join(__dirname, './token.json'))
 
-      // if(!app.username)
-        // app.username = yield prompt('username: ')
+      app.credentials = Object.assign({}, consumerInfo, token)
 
-      // app.password = yield prompt.password('password: ')
+      if(!app.username)
+        app.username = yield prompt('username: ')
+
+      app.password = yield prompt.password('password: ')
 
       // processDirectory(directory)
       // authenticate()
@@ -45,32 +44,12 @@ function uploadFile(path){
   console.log(path)
 }
 
-function authenticate(andThen){
-  console.log('building auth request')
-  var oauth = new OAuth.OAuth(
-    'https://www.tumblr.com/oauth/request_token',
-    'https://www.tumblr.com/oauth/access_token',
-    consumerInfo.key,
-    consumerInfo.secret,
-    '1.0A',
-    null,
-    'HMAC-SHA1'
-  )
-  console.log('getting')
-  oauth.get(
-    'https://www.tumblr.com/oauth/authorize',
-    app.username,
-    app.password,
-    function(e, data, res){
-      console.log(res)
-    }
-  )
-}
-
 function simple() {
-  var tumb = new t2({consumerKey: app.consumer_key, consumerSecret: app.consumer_secret})
-  tumb.get('/info', {hostname: 'aetherstragic.tumblr.com'}, function(err, json){
-    console.log(err)
-    console.log(json)
-  })
+  // console.log(app.credentials)
+  var client = tumblr.createClient(app.credentials);
+
+  // Make the request
+  client.userInfo(function (err, data) {
+    console.log(data)
+  });
 }
